fe367d0ab54b5d5c981e068ae756eceac0e70399
Vimalathithan D <110659325+vimalathithan17@users.noreply.github.com>
2025-09-21 08:46:34 +0000

    Add multiclass quantum classifiers with various methods

---
+++ qml_models.py
@@ -0,0 +1,218 @@
+import pennylane as qml
+from pennylane import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.metrics import accuracy_score
+
+# --- Models for Approach 1 (Classical Preprocessing + QML) ---
+
+class MulticlassQuantumClassifierDR(BaseEstimator, ClassifierMixin):
+    """Multiclass VQC for pre-processed, dimensionally-reduced data."""
+    def __init__(self, n_qubits=8, n_layers=3, n_classes=3, learning_rate=0.1, steps=50):
+        self.n_qubits = n_qubits
+        self.n_layers = n_layers
+        self.n_classes = n_classes
+        self.learning_rate = learning_rate
+        self.steps = steps
+        assert self.n_qubits >= self.n_classes, "Number of qubits must be >= number of classes."
+        self.dev = qml.device("default.qubit", wires=self.n_qubits)
+        self.weights = np.random.uniform(0, 2 * np.pi, (self.n_layers, self.n_qubits), requires_grad=True)
+
+    def _get_circuit(self):
+        @qml.qnode(self.dev, interface='autograd')
+        def qcircuit(inputs, weights):
+            qml.AngleEmbedding(inputs, wires=range(self.n_qubits))
+            qml.BasicEntanglerLayers(weights, wires=range(self.n_qubits))
+            return [qml.expval(qml.PauliZ(i)) for i in range(self.n_classes)]
+        return qcircuit
+
+    def _softmax(self, x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=0)
+
+    def fit(self, X, y):
+        qcircuit = self._get_circuit()
+        y_one_hot = np.eye(self.n_classes)[y]
+        opt = qml.AdamOptimizer(self.learning_rate)
+        
+        for step in range(self.steps):
+            def cost(weights):
+                raw_predictions = np.array([qcircuit(x, weights) for x in X])
+                probabilities = np.array([self._softmax(p) for p in raw_predictions])
+                # Use cross-entropy loss for multiclass
+                loss = -np.mean(y_one_hot * np.log(probabilities + 1e-9))
+                return loss
+            self.weights = opt.step(cost, self.weights)
+        return self
+
+    def predict_proba(self, X):
+        qcircuit = self._get_circuit()
+        raw_predictions = np.array([qcircuit(x, self.weights) for x in X])
+        return np.array([self._softmax(p) for p in raw_predictions])
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=1)
+
+class MulticlassQuantumClassifierDataReuploadingDR(BaseEstimator, ClassifierMixin):
+    """Data Re-uploading Multiclass VQC for pre-processed, dense data."""
+    def __init__(self, n_qubits=8, n_layers=3, n_classes=3, learning_rate=0.1, steps=50):
+        self.n_qubits = n_qubits
+        self.n_layers = n_layers
+        self.n_classes = n_classes
+        self.learning_rate = learning_rate
+        self.steps = steps
+        assert self.n_qubits >= self.n_classes, "Number of qubits must be >= number of classes."
+        self.dev = qml.device("default.qubit", wires=self.n_qubits)
+        self.weights = np.random.uniform(0, 2 * np.pi, (self.n_layers, self.n_qubits), requires_grad=True)
+
+    def _get_circuit(self):
+        @qml.qnode(self.dev, interface='autograd')
+        def qcircuit(inputs, weights):
+            for layer in range(self.n_layers):
+                qml.AngleEmbedding(inputs, wires=range(self.n_qubits))
+                qml.BasicEntanglerLayers(weights[layer], wires=range(self.n_qubits))
+            return [qml.expval(qml.PauliZ(i)) for i in range(self.n_classes)]
+        return qcircuit
+
+    def _softmax(self, x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=0)
+
+    def fit(self, X, y):
+        qcircuit = self._get_circuit()
+        y_one_hot = np.eye(self.n_classes)[y]
+        opt = qml.AdamOptimizer(self.learning_rate)
+        for step in range(self.steps):
+            def cost(weights):
+                raw_predictions = np.array([qcircuit(x, weights) for x in X])
+                probabilities = np.array([self._softmax(p) for p in raw_predictions])
+                loss = -np.mean(y_one_hot * np.log(probabilities + 1e-9))
+                return loss
+            self.weights = opt.step(cost, self.weights)
+        return self
+
+    def predict_proba(self, X):
+        qcircuit = self._get_circuit()
+        raw_predictions = np.array([qcircuit(x, self.weights) for x in X])
+        return np.array([self._softmax(p) for p in raw_predictions])
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=1)
+
+# --- Models for Approach 2 (Conditional Encoding on Selected Features) ---
+
+class ConditionalMulticlassQuantumClassifierFS(BaseEstimator, ClassifierMixin):
+    """Conditional Multiclass QVC that expects pre-processed tuple input."""
+    def __init__(self, n_qubits=8, n_layers=3, n_classes=3, learning_rate=0.1, steps=50):
+        self.n_qubits = n_qubits
+        self.n_layers = n_layers
+        self.n_classes = n_classes
+        self.learning_rate = learning_rate
+        self.steps = steps
+        assert self.n_qubits >= self.n_classes, "Number of qubits must be >= number of classes."
+        self.dev = qml.device("default.qubit", wires=self.n_qubits)
+        self.weights_ansatz = np.random.uniform(0, 2 * np.pi, (self.n_layers, self.n_qubits), requires_grad=True)
+        self.weights_missing = np.random.uniform(0, 2 * np.pi, self.n_qubits, requires_grad=True)
+
+    def _get_circuit(self):
+        @qml.qnode(self.dev, interface='autograd')
+        def qcircuit(features, is_missing_mask, weights_ansatz, weights_missing):
+            for i in range(self.n_qubits):
+                if is_missing_mask[i] == 1:
+                    qml.RY(weights_missing[i], wires=i)
+                else:
+                    qml.RY(features[i] * np.pi, wires=i)
+            qml.BasicEntanglerLayers(weights_ansatz, wires=range(self.n_qubits))
+            return [qml.expval(qml.PauliZ(i)) for i in range(self.n_classes)]
+        return qcircuit
+
+    def _softmax(self, x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=0)
+
+    def fit(self, X, y):
+        X_scaled, is_missing_mask = X
+        y_one_hot = np.eye(self.n_classes)[y]
+        qcircuit = self._get_circuit()
+        opt = qml.AdamOptimizer(self.learning_rate)
+        trainable_params = [self.weights_ansatz, self.weights_missing]
+        for step in range(self.steps):
+            def cost(params):
+                w_ansatz, w_missing = params
+                raw_predictions = np.array([qcircuit(f, m, w_ansatz, w_missing) for f, m in zip(X_scaled, is_missing_mask)])
+                probabilities = np.array([self._softmax(p) for p in raw_predictions])
+                loss = -np.mean(y_one_hot * np.log(probabilities + 1e-9))
+                return loss
+            trainable_params = opt.step(cost, trainable_params)
+        self.weights_ansatz, self.weights_missing = trainable_params
+        return self
+
+    def predict_proba(self, X):
+        X_scaled, is_missing_mask = X
+        qcircuit = self._get_circuit()
+        raw_predictions = np.array([qcircuit(f, m, self.weights_ansatz, self.weights_missing) for f, m in zip(X_scaled, is_missing_mask)])
+        return np.array([self._softmax(p) for p in raw_predictions])
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=1)
+
+    def score(self, X, y):
+        return accuracy_score(y, self.predict(X))
+
+class ConditionalMulticlassQuantumClassifierDataReuploadingFS(BaseEstimator, ClassifierMixin):
+    """Data Re-uploading Conditional Multiclass QVC."""
+    def __init__(self, n_qubits=8, n_layers=3, n_classes=3, learning_rate=0.1, steps=50):
+        self.n_qubits = n_qubits
+        self.n_layers = n_layers
+        self.n_classes = n_classes
+        self.learning_rate = learning_rate
+        self.steps = steps
+        assert self.n_qubits >= self.n_classes, "Number of qubits must be >= number of classes."
+        self.dev = qml.device("default.qubit", wires=self.n_qubits)
+        self.weights_ansatz = np.random.uniform(0, 2 * np.pi, (self.n_layers, self.n_qubits), requires_grad=True)
+        self.weights_missing = np.random.uniform(0, 2 * np.pi, (self.n_layers, self.n_qubits), requires_grad=True)
+
+    def _get_circuit(self):
+        @qml.qnode(self.dev, interface='autograd')
+        def qcircuit(features, is_missing_mask, weights_ansatz, weights_missing):
+            for layer in range(self.n_layers):
+                for i in range(self.n_qubits):
+                    if is_missing_mask[i] == 1:
+                        qml.RY(weights_missing[layer, i], wires=i)
+                    else:
+                        qml.RY(features[i] * np.pi, wires=i)
+                qml.BasicEntanglerLayers(weights_ansatz[layer], wires=range(self.n_qubits))
+            return [qml.expval(qml.PauliZ(i)) for i in range(self.n_classes)]
+        return qcircuit
+
+    def _softmax(self, x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=0)
+
+    def fit(self, X, y):
+        X_scaled, is_missing_mask = X
+        y_one_hot = np.eye(self.n_classes)[y]
+        qcircuit = self._get_circuit()
+        opt = qml.AdamOptimizer(self.learning_rate)
+        trainable_params = [self.weights_ansatz, self.weights_missing]
+        for step in range(self.steps):
+            def cost(params):
+                w_ansatz, w_missing = params
+                raw_predictions = np.array([qcircuit(f, m, w_ansatz, w_missing) for f, m in zip(X_scaled, is_missing_mask)])
+                probabilities = np.array([self._softmax(p) for p in raw_predictions])
+                loss = -np.mean(y_one_hot * np.log(probabilities + 1e-9))
+                return loss
+            trainable_params = opt.step(cost, trainable_params)
+        self.weights_ansatz, self.weights_missing = trainable_params
+        return self
+
+    def predict_proba(self, X):
+        X_scaled, is_missing_mask = X
+        qcircuit = self._get_circuit()
+        raw_predictions = np.array([qcircuit(f, m, self.weights_ansatz, self.weights_missing) for f, m in zip(X_scaled, is_missing_mask)])
+        return np.array([self._softmax(p) for p in raw_predictions])
+
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=1)
+        
+    def score(self, X, y):
+        return accuracy_score(y, self.predict(X))
