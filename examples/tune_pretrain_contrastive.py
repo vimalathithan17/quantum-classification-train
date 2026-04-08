154686fd26c91de9d65cea2c292637a9043bdde9
Vimalathithan D <110659325+vimalathithan17@users.noreply.github.com>
2026-04-08 05:37:51 +0000

    tune pce updated

--- examples/tune_pretrain_contrastive.py
+++ examples/tune_pretrain_contrastive.py
@@ -197,9 +197,10 @@ def objective(trial, args, data, modality_dims, labels, case_ids, device):
                 transformer_num_heads = 2
                 break
     
-    batch_size = trial.suggest_categorical('batch_size', [32, 64, 128])
     if args.full_batch:
         batch_size = -1
+    else:
+        batch_size = trial.suggest_categorical('batch_size', [32, 64, 128])
     
     log.info(f"Trial {trial.number} params: temp={temperature:.4f}, lr={lr:.6f}, "
              f"wd={weight_decay:.6f}, proj_dim={projection_dim}, embed_dim={embed_dim}")
