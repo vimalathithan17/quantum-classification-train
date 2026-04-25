5fa7fbae75aed825ed4cb47c0b90420bc8116621
Vimalathithan D <110659325+vimalathithan17@users.noreply.github.com>
2026-04-25 17:45:19 +0530

    Update mlp_meta_learner.py

--- mlp_meta_learner.py
+++ mlp_meta_learner.py
@@ -260,9 +260,9 @@ def main():
             pass
 
     # Create a single Train/Validation split to be used for all trials
-    log.info("Creating internal validation split (10%) for trial training and early stopping.")
+    log.info("Creating internal validation split (20%) for trial training and early stopping.")
     X_train_split, X_val_split, y_train_split, y_val_split = train_test_split(
-        X_meta_train, y_meta_train, test_size=0.10, stratify=y_meta_train, random_state=RANDOM_STATE
+        X_meta_train, y_meta_train, test_size=0.20, stratify=y_meta_train, random_state=RANDOM_STATE
     )
 
     def single_run_objective(trial):
